2b8e8d1a84e4421a6e19391bcb5512e7d735cf0f
Farhod <farhodjonhikmatullayev@gmail.com>
2024-10-31 09:24:54 +0500

    modified

--- keyboards/default/all_ranks.py
+++ keyboards/default/all_ranks.py
@@ -7,7 +7,7 @@ async def all_ranks_default_keyboard():
     ranks = await db.select_all_ranks()
     markup = ReplyKeyboardMarkup()
     markup.resize_keyboard = True
-    markup.row_width = 1
+    markup.row_width = 2
     for branch in ranks:
         text_button = branch['name']
         markup.insert(KeyboardButton(text_button))
